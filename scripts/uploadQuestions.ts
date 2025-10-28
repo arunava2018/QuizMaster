2379e0e9e1d31c01768b1d478aa9ff8908129d49
Arunava Banerjee <100664086+arunava2018@users.noreply.github.com>
2025-10-28 22:42:26 +0530

    chore: comment out question seeding logic for future reference

--- scripts/uploadQuestions.ts
+++ scripts/uploadQuestions.ts
@@ -1,25 +1,25 @@
-import "dotenv/config";
-import { Question } from "@/models/Question.js";
-import { connectDB } from "@/lib/db"
-// Enter Topic ID
-const TOPIC_ID = "";
+// import "dotenv/config";
+// import { Question } from "@/models/Question.js";
+// import { connectDB } from "@/lib/db"
+// // Enter Topic ID
+// const TOPIC_ID = "";
 
-// Enter questions array
-const questions = [
+// // Enter questions array
+// const questions = [
  
-];
+// ];
 
 
 
 
 
-const seedQuestions = async () => {
-  try {
-    await connectDB();
-    await Question.insertMany(questions);
-    console.log("Questions inserted successfully!");
-  } catch (error) {
-    console.error("Error seeding questions:", error);
-  }
-};
-seedQuestions();
+// const seedQuestions = async () => {
+//   try {
+//     await connectDB();
+//     await Question.insertMany(questions);
+//     console.log("Questions inserted successfully!");
+//   } catch (error) {
+//     console.error("Error seeding questions:", error);
+//   }
+// };
+// seedQuestions();
